8ca4c1d6928e77014ebc3dd35eface422f364f71
Navid Shad <navidshad72@gmail.com>
2022-08-01 00:19:13 +0300

    [ Task ] Implement translate service

---
+++ src/common/services/translate.service.ts
@@ -0,0 +1,27 @@
+import { Dictionary } from "../types/general.type";
+
+export class TranslateService {
+  static instance = new TranslateService();
+
+  targetLanguage = "fa";
+
+  async translate(text: string | string[]) {
+    let key = "AIzaSyCzR9jH7EGCHgvfHXJxM0997UmuwiSRkH0";
+    let url = `https://translation.googleapis.com/language/translate/v2?key=${key}`;
+
+    let body = {
+      q: text,
+      target: this.targetLanguage,
+    }
+    
+    return fetch(url, {
+      body: JSON.stringify(body),
+      method: 'POST'
+    })
+      .then((res) => res.json() as Promise<Dictionary>)
+      .then((body:Dictionary) => body.data.translations)
+      .then(list => {
+        return list.map((item: { translatedText: string; }) => item.translatedText) as string[]
+      })
+  }
+}
