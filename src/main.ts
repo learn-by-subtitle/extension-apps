63c661d1930533dc0ccd9709ec28558ddbd299c4
Navid Shad <navidshad72@gmail.com>
2022-08-07 13:24:57 +0300

    [ Task ] Add fullscreen support for netflix

--- src/main.ts
+++ src/main.ts
@@ -13,32 +13,3 @@ Object.keys(components).forEach(name => {
 })
 
 NetflixInitializer(vueApp);
-// vueApp.mount("#app");
-
-// document.addEventListener("keyup", (event) => {
-//   if (event.code != "Space") return;
-
-//   let list = document.querySelectorAll(
-//     ".player-timedtext-text-container span "
-//   );
-
-//   console.log(list.length);
-
-//   list.forEach((item) => {
-//     if (item.childElementCount == 0) {
-//       console.log(item.textContent);
-//       createSpanForWords(item);
-//     }
-//   });
-// });
-
-// function createSpanForWords(parentTag: Element) {
-//   let words = parentTag.textContent!.split(" ");
-
-//   words.forEach((word, i) => {
-//     words[i] = `<span class="word">${word}</span>`;
-//   });
-
-//   debugger;
-//   parentTag.innerHTML = words.join(" ");
-// }
--- src/module/netflix/initializer.ts
+++ src/module/netflix/initializer.ts
@@ -3,20 +3,16 @@ import { AppInitializer } from "../../common/types/general.type";
 import { SUBTITLE_CLASS } from "../../config/static";
 
 export const NetflixInitializer: AppInitializer = async (app) => {
-//   await waitUntil(() => !!document.querySelector(SUBTITLE_CLASS));
+  await waitUntil(() => !!document.querySelector(SUBTITLE_CLASS));
 
-//   let parentNode = document.querySelector("video")?.parentNode;
-//   let appDiv = document.createElement("div");
+  let appDiv = document.createElement("div");
+  let videoContainer = document.querySelector(".watch-video");
 
-//   parentNode?.appendChild(appDiv);
+  videoContainer?.insertBefore(appDiv, videoContainer.firstChild);
 
-let appDiv = document.createElement("div");
-document.body.append(appDiv);
+  appDiv.id = "app";
+  appDiv.style.position = "relative";
+  appDiv.style.zIndex = "9999";
 
-appDiv.id = "app";
-appDiv.style.position = "relative";
-appDiv.style.zIndex = "9999";
-
-app.mount(appDiv);
-
+  app.mount(appDiv);
 };
