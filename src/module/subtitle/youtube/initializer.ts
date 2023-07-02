77b29f17d34ff337df5bef85cdeebba056bf17dd
Navid Shad <navidshad72@gmail.com>
2023-07-02 20:23:27 +0300

    Add support template

--- src/module/subtitle/youtube/initializer.ts
+++ src/module/subtitle/youtube/initializer.ts
@@ -3,6 +3,7 @@ import { AppInitializer } from "../../../common/types/general.type";
 import { SUBTILE_CONTAINER_CLASS } from "./static";
 
 import youtubeComponent from "./Index.vue";
+import { analytic } from "../../../plugins/mixpanel";
 
 export const youtube: AppInitializer = {
   website: {
@@ -12,6 +13,10 @@ export const youtube: AppInitializer = {
   component: youtubeComponent as any,
   start: async (app) => {
     await waitUntil(() => !!document.querySelector(SUBTILE_CONTAINER_CLASS));
+
+    // Init Analytic
+    //
+    analytic.track('Youtube')
 
     let appDiv = document.createElement("div");
     appDiv.id = "app";
